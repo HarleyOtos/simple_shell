f9c7c69edbe96d820eb991682ac2d60bc8f03288
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-11-26 20:59:06 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -4,7 +4,11 @@
  * lsh_num_builtins - List of builtin commands, followed
  * by their corresponding functions.
  */
-char *newBuilt[] = { NULL };
+char *newBuilt[] = { NULL,
+	&lsh_cd,
+	&lsh_help,
+	&lsh_exit 
+};
 
 int lsh_num_builtins(void)
 {
